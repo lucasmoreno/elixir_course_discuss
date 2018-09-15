88e2e754ca6e73870f4cdb109961e9f91f2eafc0
Lucas Moreno Silva <lucas@thinkseg.com>
2018-09-15 18:59:23 -0300

    Add ueberauth

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [default_scope: "user,public_repo"] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "8447aeb22b4f30f2b5c5",
+  client_secret: "e643b8c1fb549271d602fe86636d8175db7a7563"
